c7c52bbe36645ac714d1a1947e00b31205a13b77
aboymond <aboymond@student.42lausanne.ch>
2023-01-27 16:52:31 +0100

    ex05

--- module_01/ex04/ft_replace.cpp
+++ module_01/ex04/ft_replace.cpp
@@ -2,9 +2,15 @@
 #include "ft_replace.hpp"
 
 std::string		ft_replace(std::string word, std::string word_rep, std::string tmp){
+	int	ret;
 
-	if (word.find(tmp)){
-		std::cout << "replace word OK" << std::endl;
+	ret = tmp.find(word);
+	while (ret != std::string::npos) {
+		std::cout << "Before ->\t" << tmp << std::endl;
+		tmp.erase(ret, word.length());
+		tmp.insert(ret, word_rep);
+		ret = tmp.find(word);
+		std::cout << "After  ->\t" << tmp << std::endl;
 	}
-	return word_rep;
+	return (tmp);
 }
--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -8,15 +8,19 @@ int	main(int argc, char **argv){
 		return (0);
 	}
 
-	std::ifstream fd(argv[1]);
-	std::string	repl;
-	repl = (std::string)argv[1] + ".replace";
-	std::ofstream output(repl);
-	std::string	tmp;
-	while (getline(fd, tmp)){
-
-		std::cout << tmp << std::endl;
-		tmp = ft_replace((std::string)argv[2], (std::string)argv[3], tmp);
-		output << tmp << std::endl;
+	std::string		old_file = (std::string)argv[1];
+	std::string		new_file = old_file + ".replace";
+	std::ifstream	fd(old_file);
+	std::ofstream	output(new_file);
+	std::string		buf;
+	if (fd.is_open() == false){
+		fd.close();
+		return (0);
 	}
+	else{
+		while (getline(fd, buf)){
+			output << ft_replace((std::string)argv[2], (std::string)argv[3], buf) << std::endl;
+		}
+	}
+	fd.close();
 }
