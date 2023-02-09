46e93f1d644c036e245af81e7fee59d69e9dd419
aboymond <aboymond@student.42lausanne.ch>
2023-02-09 17:25:44 +0100

    m2_ex02

--- module_01/ex04/ft_replace.cpp
+++ module_01/ex04/ft_replace.cpp
@@ -2,15 +2,16 @@
 #include "ft_replace.hpp"
 
 std::string		ft_replace(std::string word, std::string word_rep, std::string tmp){
-	int	ret;
+	int	ret = 0;
 
-	ret = tmp.find(word);
+	ret = tmp.find(word, ret);
 	while (ret != std::string::npos) {
 		std::cout << "Before ->\t" << tmp << std::endl;
 		tmp.erase(ret, word.length());
 		tmp.insert(ret, word_rep);
-		ret = tmp.find(word);
+		ret = tmp.find(word, ret+1);
 		std::cout << "After  ->\t" << tmp << std::endl;
+	//exit (0);
 	}
 	return (tmp);
 }
